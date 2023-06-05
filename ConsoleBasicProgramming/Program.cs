7ed63644694c3527318a23809a6f7897061fa7a1
Tabish Ahmad <tabish0211@hotmail.com>
2023-06-05 09:34:41 +0530

    Add project files.

---
+++ ConsoleBasicProgramming/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ConsoleBasicProgramming
+{
+    //class Program
+    //{
+    //    //entry point method ---Main is not going to send any response to its caller--void no return
+    //    //CLR--Common language runtime is the caller of the main
+    //    static void Main()
+    //    {
+    //        //How to print a message on to console
+    //        //Hello world
+    //        //Console.WriteLIne
+    //        //Console.Write("Please enter username \n ");
+    //        //Console.WriteLine("Please enter username");
+    //        Console.WriteLine("Please enter username");
+
+
+    //        //Take string--(group of character) input and display that input on to console
+    //        //tabish----Hey tabish how are you?
+    //        //variable is nothing but a temp memory to store the user , because you can store any data of that type
+    //        //
+
+    //        string name = Console.ReadLine();//input//Tabish or Uzma xyz
+    //                                         // Console.WriteLine(name);
+    //        //Console.WriteLine();
+    //        //Console.WriteLine("Hey {0} how are you?", name);
+    //        ////Console.WriteLine($"Hey {name} how are you?");
+    //        //Console.ReadLine();     //Take string--(group of character) input and display that input on to console
+    //        //                        //tabish----Hey tabish how are you?
+    //                                //variable is nothing but a temp memory to store the user , because you can store any data of that type
+    //                                //
+
+    //      // string name = Console.ReadLine();
+    //        //input//Tabish or Uzma xyz
+    //        // Console.WriteLine(name);
+    //        Console.WriteLine("Hey "+name+ " how are you?");
+    //       // Console.WriteLine("Hey {0} how are you?", name);
+    //        //Console.WriteLine($"Hey {name} how are you?");
+    //        Console.ReadLine();
+
+    //    }
+    //}
+
+
+}
+
+
