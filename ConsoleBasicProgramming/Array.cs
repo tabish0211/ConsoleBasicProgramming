144d852d4af58c0c805b7fb2abe111890ff8f62d
Tabish Ahmad <tabish0211@hotmail.com>
2023-06-22 19:59:26 +0530

    Array samples

---
+++ ConsoleBasicProgramming/Array.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleBasicProgramming
+{
+    class ArrayDemo
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter the size of an array");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            int[] rollNumber = new int[size];
+
+            for (int i = 0; i < rollNumber.Length; i++)
+            {
+                Console.WriteLine($"Enter the roll number {i + 1}");
+                rollNumber[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+            ////1,2,3,4
+            //Console.WriteLine("************output without reverse********************");
+            //for (int i = 0; i < rollNumber.Length; i++)
+            //{
+            //    Console.WriteLine(rollNumber[i]);
+            //}//1,2,3,4---i=0---1-----i=3---4
+            // //4,3,2,1--i=0--4
+
+            ////Console.WriteLine("************reverse array********************");
+
+            ////Array.Reverse(rollNumber);
+            ////rollNumber=rollNumber.Reverse().ToArray();
+
+            ////for (int i = 0; i < rollNumber.Length; i++)
+            ////{
+            ////    Console.WriteLine(rollNumber[i]);
+            ////}
+
+            ////sorting---ascending ---descending
+
+            //Console.WriteLine("************Sorted in ascending/descending order array********************");
+            //Array.Sort(rollNumber);//ascedning
+            //Array.Reverse(rollNumber);//descneding
+            //for (int i = 0; i < rollNumber.Length; i++)
+            //{
+            //    Console.WriteLine(rollNumber[i]);
+            //}
+
+
+            foreach (int item in rollNumber)//
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
--- ConsoleBasicProgramming/LoopingControl.cs
+++ ConsoleBasicProgramming/LoopingControl.cs
@@ -1,95 +1,95 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace ConsoleBasicProgramming
-{
-    class LoopingControl
-    {
-        static void Main()
-        {
-            //Looping --repitive task ---
-            //for,while,do-while,foreach
-            //2 table
-            //int x = 1;
-            //Console.WriteLine(2 * x);
-            //x = 2;
-            //Console.WriteLine(2 * x);
-            //x = 3;
-            //Console.WriteLine(2 * x);
+//namespace ConsoleBasicProgramming
+//{
+//    class LoopingControl
+//    {
+//        static void Main()
+//        {
+//            //Looping --repitive task ---
+//            //for,while,do-while,foreach
+//            //2 table
+//            //int x = 1;
+//            //Console.WriteLine(2 * x);
+//            //x = 2;
+//            //Console.WriteLine(2 * x);
+//            //x = 3;
+//            //Console.WriteLine(2 * x);
 
-            //int x = 1;
-            ////x = x + 1;
-            ////x += 1;
-            ////x = x++;//post incremnet  x+1
-            ////x = ++x;//x+1
+//            //int x = 1;
+//            ////x = x + 1;
+//            ////x += 1;
+//            ////x = x++;//post incremnet  x+1
+//            ////x = ++x;//x+1
 
-            //int y = x + x++ + ++x + x++;
-            //Console.WriteLine(y);//5
-            //Console.WriteLine(x);//3
+//            //int y = x + x++ + ++x + x++;
+//            //Console.WriteLine(y);//5
+//            //Console.WriteLine(x);//3
 
-            //assignment -input --number 
-            //for (int x = 1; x < 11; x++)
-            //{
-            //    Console.WriteLine($"2*{x}={2*x}");
-            //}
+//            //assignment -input --number 
+//            //for (int x = 1; x < 11; x++)
+//            //{
+//            //    Console.WriteLine($"2*{x}={2*x}");
+//            //}
 
-            //
-           // int x = 1;
-            //do
-            //{
-            //    //task
-            //    Console.WriteLine($"2*{x}={2 * x}");
-            //    x++;
-            //} while (x < 11);//Bottom testing 
+//            //
+//           // int x = 1;
+//            //do
+//            //{
+//            //    //task
+//            //    Console.WriteLine($"2*{x}={2 * x}");
+//            //    x++;
+//            //} while (x < 11);//Bottom testing 
 
-            //Top testing
-            //while (x < 11)
-            //{
-            //    Console.WriteLine($"2*{x}={2 * x}");
-            //    x++;
-            //}
+//            //Top testing
+//            //while (x < 11)
+//            //{
+//            //    Console.WriteLine($"2*{x}={2 * x}");
+//            //    x++;
+//            //}
 
-            ////when you know number of times
-            //for (int i = 0; i < length; i++)
-            //{
+//            ////when you know number of times
+//            //for (int i = 0; i < length; i++)
+//            //{
 
-            //}
+//            //}
 
-            ////when you dont know the how much times loop should execute 
-            //while (true)
-            //{
+//            ////when you dont know the how much times loop should execute 
+//            //while (true)
+//            //{
 
-            //}
+//            //}
 
 
-            //array-?
-            //foreach (var item in collection)
-            //{
+//            //array-?
+//            //foreach (var item in collection)
+//            //{
 
-            //}
+//            //}
 
-            //for (int i = 1; i <= 10; i++)
-            //{
-            //    if (i==6)
-            //    {
-            //        break;//forcefully loop or curly brace break
-            //    }
-            //    Console.WriteLine(i);
-            //}
+//            //for (int i = 1; i <= 10; i++)
+//            //{
+//            //    if (i==6)
+//            //    {
+//            //        break;//forcefully loop or curly brace break
+//            //    }
+//            //    Console.WriteLine(i);
+//            //}
 
-            for (int i = 1; i <= 10; i++)
-            {
-                if (i == 6)
-                {
-                    continue;//skip
-                }
-                Console.WriteLine(i);
-            }
-            Console.ReadLine();
+//            for (int i = 1; i <= 10; i++)
+//            {
+//                if (i == 6)
+//                {
+//                    continue;//skip
+//                }
+//                Console.WriteLine(i);
+//            }
+//            Console.ReadLine();
 
-        }
-    }
-}
+//        }
+//    }
+//}
