a4676cf16c6b36ea3587974b0f69b17b56aa4c93
Tabish Ahmad <tabish0211@hotmail.com>
2023-07-03 09:51:01 +0530

    Methods Demo

--- ConsoleBasicProgramming/Array.cs
+++ ConsoleBasicProgramming/Array.cs
@@ -8,55 +8,55 @@ namespace ConsoleBasicProgramming
 {
     class ArrayDemo
     {
-        static void Main()
-        {
+        //static void Main()
+        //{
           
-            Console.WriteLine("Enter the size of an array");
-            int size = Convert.ToInt32(Console.ReadLine());
+        //    Console.WriteLine("Enter the size of an array");
+        //    int size = Convert.ToInt32(Console.ReadLine());
 
-            int[] rollNumber = new int[size];
+        //    int[] rollNumber = new int[size];
 
-            for (int i = 0; i < rollNumber.Length; i++)
-            {
-                Console.WriteLine($"Enter the roll number {i + 1}");
-                rollNumber[i] = Convert.ToInt32(Console.ReadLine());
+        //    for (int i = 0; i < rollNumber.Length; i++)
+        //    {
+        //        Console.WriteLine($"Enter the roll number {i + 1}");
+        //        rollNumber[i] = Convert.ToInt32(Console.ReadLine());
 
-            }
-            ////1,2,3,4
-            //Console.WriteLine("************output without reverse********************");
-            //for (int i = 0; i < rollNumber.Length; i++)
-            //{
-            //    Console.WriteLine(rollNumber[i]);
-            //}//1,2,3,4---i=0---1-----i=3---4
-            // //4,3,2,1--i=0--4
+        //    }
+        //    ////1,2,3,4
+        //    //Console.WriteLine("************output without reverse********************");
+        //    //for (int i = 0; i < rollNumber.Length; i++)
+        //    //{
+        //    //    Console.WriteLine(rollNumber[i]);
+        //    //}//1,2,3,4---i=0---1-----i=3---4
+        //    // //4,3,2,1--i=0--4
 
-            ////Console.WriteLine("************reverse array********************");
+        //    ////Console.WriteLine("************reverse array********************");
 
-            ////Array.Reverse(rollNumber);
-            ////rollNumber=rollNumber.Reverse().ToArray();
+        //    ////Array.Reverse(rollNumber);
+        //    ////rollNumber=rollNumber.Reverse().ToArray();
 
-            ////for (int i = 0; i < rollNumber.Length; i++)
-            ////{
-            ////    Console.WriteLine(rollNumber[i]);
-            ////}
+        //    ////for (int i = 0; i < rollNumber.Length; i++)
+        //    ////{
+        //    ////    Console.WriteLine(rollNumber[i]);
+        //    ////}
 
-            ////sorting---ascending ---descending
+        //    ////sorting---ascending ---descending
 
-            //Console.WriteLine("************Sorted in ascending/descending order array********************");
-            //Array.Sort(rollNumber);//ascedning
-            //Array.Reverse(rollNumber);//descneding
-            //for (int i = 0; i < rollNumber.Length; i++)
-            //{
-            //    Console.WriteLine(rollNumber[i]);
-            //}
+        //    //Console.WriteLine("************Sorted in ascending/descending order array********************");
+        //    //Array.Sort(rollNumber);//ascedning
+        //    //Array.Reverse(rollNumber);//descneding
+        //    //for (int i = 0; i < rollNumber.Length; i++)
+        //    //{
+        //    //    Console.WriteLine(rollNumber[i]);
+        //    //}
 
 
-            foreach (int item in rollNumber)//
-            {
-                Console.WriteLine(item);
-            }
-            Console.ReadLine();
+        //    foreach (int item in rollNumber)//
+        //    {
+        //        Console.WriteLine(item);
+        //    }
+        //    Console.ReadLine();
 
-        }
+        //}
     }
 }
---
+++ ConsoleBasicProgramming/MethdosDemo.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleBasicProgramming
+{
+    class MethdosDemo
+    {
+
+        //static methods call static method directly
+        //non static method---? It call via creating object
+        static void Main()
+        {
+
+
+            int s;
+            int z=4;
+
+            //Sum_withOutParam(out s);
+            Sum_withMorethanoneOutParam(out s, out z);
+            Console.WriteLine($"summation={s} and diff ={z}");
+
+            Console.ReadLine();
+
+        }
+
+
+        //No-Return--Sum is not going to retrun anything to the Main
+        static void Sum()
+        {
+
+            int x, y, s = 0;
+            x = 1;
+            y = 2;
+            s = x + y;
+            Console.WriteLine(s);
+
+        }
+
+        static int  Sum_withRetrun()
+        {
+
+            int x, y, s = 0;
+            x = 1;
+            y = 2;
+            s = x + y;
+
+            return s;
+            
+
+        }
+
+        //Pass by value
+        static int Sum_withRetrunWithParams(int x,int y)
+        {
+            int s = x + y;
+            return s;
+
+        }
+
+        //Pass by reference---1001,1002
+
+        static int Sum_withRetrunWithReferenceParams(ref int x, ref int y)
+        {
+            x = 5;
+            y = 6;
+            int s = x + y;
+            
+            return s;
+        }
+
+        static void Sum_withOutParam(out int s)
+        {
+
+            int x, y;
+            x = 1;
+            y = 2;
+            s = x + y;          
+
+        }
+
+        static void Sum_withMorethanoneOutParam(out int s,out int z)
+        {
+
+            int x, y;
+            x = 1;
+            y = 2;
+            s = x + y;
+            z = x - y;
+
+        }
+    }
+}
